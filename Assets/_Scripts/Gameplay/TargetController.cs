6b8174dcb31e735a18b11e8e32200fbd59e134a9
Hovhannes <hovhan198@gmail.com>
2026-06-27 02:58:20 +0300

    [PT-3] Implement target prefab (#3)

---
+++ Assets/_Scripts/Gameplay/TargetController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+
+    [SerializeField] private float rotationSpeed = 50f;
+    [SerializeField] private bool isClockwise = true; // refactor to direction enum
+
+    private float directionMultiplier => isClockwise ? -1f : 1f;
+
+    void Update()
+    {
+      float zIndex = directionMultiplier * rotationSpeed * Time.deltaTime;
+      transform.Rotate(0f, 0f, zIndex);
+    }
+}
