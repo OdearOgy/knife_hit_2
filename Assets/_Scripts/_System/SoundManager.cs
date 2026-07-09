f3f94e3ab62f8cf0c5ad98f08e61c51221485821
Hovhannes <hovhan198@gmail.com>
2026-07-09 15:10:17 +0400

    [PT-17] Fix sfx play delay

--- Assets/_Scripts/_System/SoundManager.cs
+++ Assets/_Scripts/_System/SoundManager.cs
@@ -5,8 +5,10 @@ public class SoundManager : MonoBehaviour
   public static SoundManager Instance { get; private set; }
 
   [SerializeField] private SoundLibrary library;
-  [SerializeField] private AudioSource sfxSource;
-  [SerializeField] private AudioSource musicSource;
+  [SerializeField] private int sfxPoolSize = 4;
+
+  private AudioSource[] sfxSources;
+  private int sfxIndex = 0;
 
   private void Awake()
   {
@@ -18,17 +20,29 @@ public class SoundManager : MonoBehaviour
 
     Instance = this;
     DontDestroyOnLoad(gameObject);
+
+    sfxSources = new AudioSource[sfxPoolSize];
+    for (int i = 0; i < sfxPoolSize; i++)
+    {
+      AudioSource src = gameObject.AddComponent<AudioSource>();
+      src.playOnAwake = false;
+      sfxSources[i] = src;
+    }
   }
 
   public void PlaySFX(AudioClip clip, float volume = 1f)
   {
-    if (clip == null || sfxSource == null) return;
-    sfxSource.PlayOneShot(clip, volume);
+    if (clip == null || sfxSources == null || sfxSources.Length == 0) return;
+
+    AudioSource src = sfxSources[sfxIndex];
+    sfxIndex = (sfxIndex + 1) % sfxSources.Length;
+
+    src.clip = clip;
+    src.volume = volume;
+    src.Play();
   }
 
   public void PlayTargetHit() => PlaySFX(library?.targetHit);
   public void PlayKnifeMiss() => PlaySFX(library?.knifeMiss);
   public void PlayKnifeThrow() => PlaySFX(library?.knifeThrow);
-  // public void PlayLevelWin() => PlaySFX(library?.levelWin);
-  // public void PlayLevelLose() => PlaySFX(library?.levelLose);
 }
