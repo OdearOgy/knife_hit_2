48e9cc28899ed154e14cc8d9ba0377994803ce6b
Hovhannes <hovhan198@gmail.com>
2026-07-09 15:01:19 +0400

    [PT-17] Implement sound library for levels

--- Assets/_Scripts/_Configs/SoundLibrary.cs
+++ Assets/_Scripts/_Configs/SoundLibrary.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SoundLibrary", menuName = "Knife Hit/Sound Library")]
 public class SoundLibrary : ScriptableObject
 {
+
+  [Header("Target Hit Sound")]
   public AudioClip targetHit;
+
+  [Header("Knife clash sound")]
   public AudioClip knifeMiss;
+
+  [Header("Knife throw sound")]
   public AudioClip knifeThrow;
-  public AudioClip levelWin;
-  public AudioClip levelLose;
 }
--- Assets/_Scripts/_System/SoundManager.cs
+++ Assets/_Scripts/_System/SoundManager.cs
@@ -29,6 +29,6 @@ public class SoundManager : MonoBehaviour
   public void PlayTargetHit() => PlaySFX(library?.targetHit);
   public void PlayKnifeMiss() => PlaySFX(library?.knifeMiss);
   public void PlayKnifeThrow() => PlaySFX(library?.knifeThrow);
-  public void PlayLevelWin() => PlaySFX(library?.levelWin);
-  public void PlayLevelLose() => PlaySFX(library?.levelLose);
+  // public void PlayLevelWin() => PlaySFX(library?.levelWin);
+  // public void PlayLevelLose() => PlaySFX(library?.levelLose);
 }
